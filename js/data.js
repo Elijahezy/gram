df4584e34f4641c548af3b3fd0acd93b8e1fc048
Илья <makomck98@gmail.com>
2021-06-18 14:12:29 +0300

    Разбил код на модули

---
+++ js/data.js
@@ -0,0 +1,4 @@
+import './utils/create-photo-descriptions.js';
+import {createPhotoDescriptions, USER_PHOTO_DESCRIPTIONS} from './utils/create-photo-descriptions.js';
+
+createPhotoDescriptions(USER_PHOTO_DESCRIPTIONS);
--- js/utils/check-string-length.js
+++ js/utils/check-string-length.js
@@ -1,3 +1,5 @@
 function checkStringLength (string, length) {
   return string.length <= length;
 }
+
+export {checkStringLength};
