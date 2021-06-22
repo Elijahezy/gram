a469932ca056073a36d86be96287882152c91ea2
Илья <makomck98@gmail.com>
2021-06-22 17:42:20 +0300

    Пофиксил вывод миниатюр на страницу

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
 import {createPictureList} from './utils/create-picture-descriptions-small.js';
+import {createPictureDescriptions, USER_PICTURE_DESCRIPTIONS} from './utils/create-picture-descriptions.js';
 
-createPictureList();
+createPictureList(createPictureDescriptions(USER_PICTURE_DESCRIPTIONS));
